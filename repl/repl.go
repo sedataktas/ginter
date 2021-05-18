e7ac2480b34b3e7adae2d393889563e5ac62b693
Sedat Aktas <sedata38@gmail.com>
2021-05-18 20:02:04 +0300

    Created evaluator Added Integer, boolean and null object types Implemented Integer Literals for evaluator

--- repl/repl.go
+++ repl/repl.go
@@ -3,6 +3,7 @@ package repl
 import (
 	"bufio"
 	"fmt"
+	"ginter/evaluator"
 	"ginter/lexer"
 	"ginter/parser"
 	"io"
@@ -29,8 +30,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
