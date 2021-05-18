0e7bdc6057fa2ea3c8a5c45d04c5afe11c72fc8a
Sedat Aktas <sedata38@gmail.com>
2021-05-18 14:38:24 +0300

    minor change

--- repl/repl.go
+++ repl/repl.go
@@ -4,7 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"ginter/lexer"
-	"ginter/token"
+	"ginter/parser"
 	"io"
 )
 
@@ -12,15 +12,30 @@ const PROMPT = ">>"
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
-	for scanner.Scan() {
-		fmt.Print(PROMPT)
-		line := scanner.Text()
-		if line != "" {
-			l := lexer.New(line)
-
-			for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-				fmt.Printf("%+v\n", tok)
-			}
+	for {
+		fmt.Printf(PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
 		}
+
+		line := scanner.Text()
+		l := lexer.New(line)
+		p := parser.New(l)
+
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
+		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
