7f2d6bf258c9ff2740bbc013ed5d4100a93eb32e
Sedat Aktas <sedata38@gmail.com>
2021-05-23 22:40:24 +0300

    lexing, parsing, evaluating strings and string concatenation

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -21,6 +21,8 @@ let ten = 10;
    }
    10 == 10; 
    10 != 9;
+   "foobar"
+   "foo bar"
 `
 
 	tests := []struct {
@@ -100,6 +102,8 @@ let ten = 10;
 		{token.NOT_EQ, "!="},
 		{token.INT, "9"},
 		{token.SEMICOLON, ";"},
+		{token.STRING, "foobar"},
+		{token.STRING, "foo bar"},
 		{token.EOF, ""},
 	}
 
